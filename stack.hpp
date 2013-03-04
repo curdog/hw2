5825aa85af633ed19fe5695feb589408c3b5a8b9
bpnikehoops93 <l.miller3@hotmail.com>
2013-03-03 22:56:12 -0500

    added comments, will edit them later

--- stack.hpp
+++ stack.hpp
@@ -1,3 +1,43 @@
+// CLASS PROVIDED:  List (an ADT for a fraction)
+//
+// CONSTRUCTOR for the List class:
+//   List()
+//     Description: 	constructor will initialize List objects
+//     Preconditions: none
+//     Postcondition: creates object of type List
+//
+// MODIFICATION MEMBER FUNCTIONS for the List class
+//   void setsize (const char value)
+//     Description: 	set the size of the array
+//     Precondition: value is an integer
+//     Postcondition: pointer is set to the address of the array
+//
+//   void insert(const int value)
+//     Description: 	fills the array
+//     Precondition: value is an integer
+//     Postcondition: first slot of the array is set to num
+//
+//   void setcount  (const int value)
+//     Description: 	sets the memory location of the pointer
+//     Precondition: value is an integer
+//     Postcondition: location has been set to value
+//
+// CONSTANT MEMBER FUNCTIONS for the Fraction class
+//   int getcount (void) const
+//     Description: 	obtains the location of the pointer
+//     Preconditions: none
+//     Postcondition: count is returned
+//
+//   int getsize(void) const
+//     Description: 	obtains the size of the array
+//     Preconditions: none
+//     Postcondition: size is returned
+//
+//   void print (void) const
+//     Description: 	print the values that are in the array
+//     Preconditions: none
+//     Postcondition: values of the array are displayed on the screen
+
 #ifndef STACK_
 #define STACK_
 
@@ -11,10 +51,12 @@ class Stack{
   string pop();
   string peek();
   void push( string );
+ 
  private:
+  /*pointer to the top*/
   Node<string> *top;
 };
 
 //copy past node code here :-)
 
-#endif
+#endif /*STACK_H*/
