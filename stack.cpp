663b5a4a8e366950b4001c562db3b619d394f72d
bpnikehoops93 <l.miller3@hotmail.com>
2013-03-03 23:11:32 -0500

    added some code for the functions. its kind of all of the place, ill continue editing it

--- stack.cpp
+++ stack.cpp
@@ -2,18 +2,40 @@
 
 #include <string>
 
-Stack::Stack(){
-
+Stack::Stack()
+{
+/*initializes top to NULL*/
+  top = NULL;
 }
 
-string Stack::pop(){
+string Stack::pop()
+{
+  /*creates temp*/
+  Node *temp;
+  
+  if(top != NULL)
+  {
+    temp = top;
+    top = top->connect;
+    delete temp;
+  }
+  else 
+  cout<< "Error: You cannot remove anything from an empty list." << endl;
 
-}
+}/*pop*/
 
 string Stack::peek(){
 
 }
 
-void Stack::push( string datas ){
+void Stack::push( const Obj& new )
+{
+Node *Node1;
+/*creates new Node*/
+Node1 = new Node;
 
-}
+Node1->info = new;
+Node1->connect = top;
+/*top equals the new Node*/
+top = Node1;
+}/*push*/
